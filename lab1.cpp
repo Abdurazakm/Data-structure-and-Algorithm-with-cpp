03eb37c2bbebe1542461490e4ffa2bded8e16bf9
abdurazakm <abdulkem5472@gmail.com>
2024-11-07 15:09:54 +0300

    complet lab 1

--- lab1.cpp
+++ lab1.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-#include<algorithm>
+#include <algorithm>
 using namespace std;
-int linear_search(int list[], int key)
-{
+
+int linear_search(int list[], int key, int size) {
     int index = -1;
-    for (int i = 0; i < 7; i++)
-    {
-        if (list[i] == key)
-        {
+    for (int i = 0; i < size; i++) {
+        if (list[i] == key) {
             index = i;
             break;
         }
@@ -15,195 +13,180 @@ int linear_search(int list[], int key)
     return index;
 }
 
-int binear_search(int list[], int key)
-{
-    int n = 5;
-    int top = n - 1, bottom = 0, found = 0, index = 0, middle;
-    do
-    {
-        middle = (top - bottom) / 2;
-        if (key == list[middle])
-            found = 1;
-
-        else
-        {
-            if (key < list[middle])
-                top = middle - 1;
-            else
-                bottom = middle + 1;
+int binary_search(int list[], int key, int size) {
+    int bottom = 0, top = size - 1, middle;
+    while (bottom <= top) {
+        middle = bottom + (top - bottom) / 2;
+        if (list[middle] == key) {
+            return middle;
+        } else if (list[middle] < key) {
+            bottom = middle + 1;
+        } else {
+            top = middle - 1;
         }
-    } while (found == 0 && top >= bottom);
-
-    if (found == 0)
-        index = -1;
-    else
-        index = middle;
-    return index;
+    }
+    return -1;
 }
 
-void check(int i, int key)
-{
-    if (i == -1)
-    {
+void check(int i, int key) {
+    if (i == -1) {
         cout << key << " is not found in the list" << endl;
-    }
-    else
+    } else {
         cout << key << " found at index " << i << endl;
-}
-
-void bubble_sorting(int list[], int size_of_list){
-    for (int i = size_of_list-2; i >=0;  i--)
-    {
-        for (int j = 0; j <=i; j++)
-        {
-            if (list[j] > list[j+1])
-
-            { 
-                swap(list[j],list[j+1]);
-                }
-        }
-        
-    }
-    
-}
-
-
-void simple_sorting(int list[], int size_of_list){
-    for (int i = 0; i < size_of_list - 2; i++)
-    {
-        for (int j = i + 1; j < size_of_list - 1; j++)
-        {
-            if(list[i]>list[j])
-            swap(list[i],list[j]);
-        }
-        
     }
 }
-    
 
-void selection(int list[], int size){
-    int index;
-    int j;
-    int mnm;
-    for (int i = 0; i < size-1; i++)
-    {
-        mnm = list[i];
-        index = i;
-        for (j = i+1; j <=size-1; j++)
-        {
-            
-            if (list[i] > list[j])
-            {
-                mnm = list[j];
-                index = j;
-            }  
-        }
-        j=index;
-        list[j] = list[i];
-        list[i] = mnm;
-    }
- 
-}
-
-void insertion_sorting(int list[], int size){
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = i+1; j < size; j--)
-        {
-            if(list[j-1]>list[j]){
-                swap(list[j-1],list[j]);
+void bubble_sorting(int list[], int size_of_list) {
+    for (int i = size_of_list - 1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (list[j] > list[j + 1]) {
+                swap(list[j], list[j + 1]);
             }
-            else
-            break;
         }
-        
     }
-    
 }
 
-int main()
-{
+void simple_sorting(int list[], int size_of_list) {
+    for (int i = 0; i < size_of_list - 1; i++) {
+        for (int j = i + 1; j < size_of_list; j++) {
+            if (list[i] > list[j]) {
+                swap(list[i], list[j]);
+            }
+        }
+    }
+}
+
+void selection_sort(int list[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < size; j++) {
+            if (list[j] < list[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(list[i], list[minIndex]);
+    }
+}
+
+void insertion_sort(int list[], int size) {
+    for (int i = 1; i < size; i++) {
+        int key = list[i];
+        int j = i - 1;
+        while (j >= 0 && list[j] > key) {
+            list[j + 1] = list[j];
+            j--;
+        }
+        list[j + 1] = key;
+    }
+}
+
+// Function for Binary Insertion Sort
+void binary_insertion_sort(int list[], int size) {
+    for (int i = 1; i < size; i++) {
+        int key = list[i];
+        int bottom = 0, top = i - 1;
+
+        // Binary search to find the position for insertion
+        while (bottom <= top) {
+            int mid = bottom + (top - bottom) / 2;
+            if (list[mid] <= key) {
+                bottom = mid + 1;
+            } else {
+                top = mid - 1;
+            }
+        }
+
+        // Shift elements to make space for key
+        for (int j = i; j > bottom; j--) {
+            list[j] = list[j - 1];
+        }
+        list[bottom] = key;
+    }
+}
+
+int main() {
     int i, size, choice;
-    cout << "enter the soze of data" << endl;
+    cout << "Enter the size of the data: ";
     cin >> size;
     int list[size];
-    cout << "enter the data" << endl;
-    for (i = 0; i < size; i++)
-    {
-        cout << "enter the first " << i + 1;
+    cout << "Enter the data elements: " << endl;
+    for (i = 0; i < size; i++) {
+        cout << "Enter element " << i + 1 << ": ";
         cin >> list[i];
     }
 
-   back_to_home:
+back_to_home:
+cout<<"-----------------------"<<endl;
+cout<<"what do you want:"<<endl;
+cout<<"1 search"<<endl;
+cout<<"2 sort"<<endl;
+cout<<"3 Exit"<<endl;
+cout<<"-----------------------"<<endl;
+cin>>choice;
 
-    cout << "what do you want" << endl;
-    cout << "for search press 1\n for sort press 2" << endl;
-    cin >> choice;
-    switch (choice)
-    {
-    case 1:
-        cout << "by which method do you want" << endl;
-        cout << "for sequential press 1\n for simple press 2" << endl;
-        int searching_choice;
+
+    
+    switch (choice) {
+    case 1: {
+        cout << "Choose search method:\n1. Linear Search\n2. Binary Search" << endl;
+        int searching_choice, key;
         cin >> searching_choice;
-        switch (searching_choice)
-        {
-        case 1:
-
-            cout << "enter the number you want search" << endl;
-            int key;
-            cin >> key;
-            int searchIndex = linear_search(list, key);
-            check(searchIndex, key);
-            // break;
-        case 2:
-            cout << "enter the number you want search" << endl;
-            cin >> key;
-            searchIndex = linear_search(list, key);
-            check(searchIndex, key);
-            break;
-        case 3:
+        
+        cout << "Enter the number you want to search: ";
+        cin >> key;
+        
+        int searchIndex;
+        if (searching_choice == 1) {
+            searchIndex = linear_search(list, key, size);
+        } else if (searching_choice == 2) {
+            searchIndex = binary_search(list, key, size);
+        } else {
+            cout << "Invalid input" << endl;
             goto back_to_home;
-            break;
-
-        default:
-            cout<< "invaled input"<<endl;
-
-            
         }
-    case 2:
-
+        
+        check(searchIndex, key);
+        break;
+    }
+    case 2: {
         int sorting_choice;
-        cout << "The values before sorting are: \n";
-        for(i = 0; i < size; i++)
-            cout << list[i] << " ";
-        cout<<"by which method do you want sort data \nfor simple press 1 \n bubble press 2\ninsertion press 3\nselection press 4"<<endl;
-        cin>>sorting_choice;
-        switch (sorting_choice)
-        {
+        cout << "The values before sorting are: ";
+        for (i = 0; i < size; i++) cout << list[i] << " ";
+        cout << "\nChoose sorting method:\n1. Simple Sort\n2. Bubble Sort\n3. Insertion Sort\n4. Selection Sort\n5. Binary Insertion Sort" << endl;
+        cin >> sorting_choice;
+        
+        switch (sorting_choice) {
         case 1:
             simple_sorting(list, size);
             break;
         case 2:
-          bubble_sorting(list, size);
-          break;
-        case 3:
-          insertion_sorting(list, size);
-        case 4:
-          goto back_to_home;
-
-        default:
-            cout<< "invaled input"<<endl;
+            bubble_sorting(list, size);
             break;
+        case 3:
+            insertion_sort(list, size);
+            break;
+        case 4:
+            selection_sort(list, size);
+            break;
+        case 5:
+            binary_insertion_sort(list, size);
+            break;
+        default:
+            cout << "Invalid input" << endl;
+            goto back_to_home;
         }
-
-    case 3:
-      return 0;
-
-        break;
-
-    default:
-      cout<< "invaled input"<<endl;
+        
+        cout << "The values after sorting are: ";
+        for (i = 0; i < size; i++) cout << list[i] << " ";
+        cout << endl;
         break;
     }
+    case 3:
+        return 0;
+    default:
+        cout << "Invalid input" << endl;
+        break;
+    }
+
+    goto back_to_home;
 }
